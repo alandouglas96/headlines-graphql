15d16ac0d820c6a2cfc9967c20aeb556055d525d
alandouglas96 <alandouglas96@gmail.com>
2020-03-04 19:54:18 +0100

    MLAB secure

--- server/db.js
+++ server/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url =
-  'mongodb://alandouglas96:Luninance758@ds133328.mlab.com:33328/heroku_w18xw386';
+const url = process.env.MLAB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () =>
