a3d97838a31ea0e05d712086d0b0301fbe4f8ba5
eeden01 <mmvaneeden@gmail.com>
2020-03-05 12:08:11 +0100

    setup basic react structure

---
+++ client/src/Components/HeadLine-item/HeadLine-item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function HeadLineItem () {
+  return (
+    <div className="HeadLineItem">
+      <h1>HeadLineItem works</h1>
+    </div>
+  );
+}
+
+export default HeadLineItem;
---
+++ client/src/Components/HeadlineList/HeadLine-list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import HeadLineItem from '../HeadLine-item/HeadLine-item';
+
+function HeadLineList () {
+  return (
+    <div className="HeadLineList">
+      <h1>HeadLineList works</h1>
+      <HeadLineItem/>
+    </div>
+  );
+}
+
+export default HeadLineList;
