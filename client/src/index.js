f1a8cc83dc25320f498e466c67cf22bfe9a9acc6
lluisemper <lluisemper@gmail.com>
2020-03-05 15:43:47 +0100

    resolvers updated and object recivied in the client

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+});
+
+client
+  .query({
+    query: gql`
+    {
+      headline (newspaper: "laVanguardia"){
+        newspaper
+        headline
+      }
+    }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- server/resolvers.js
+++ server/resolvers.js
@@ -3,7 +3,10 @@ const HeadlineSchema = require("./models/headlines.js");
 const resolvers = {
   Query: {
     headline: async (_, args) => {
+      console.log(args);
+      
       return findData(args, HeadlineSchema);
+
     },
   }
 };
@@ -11,14 +14,20 @@ const resolvers = {
 module.exports = resolvers;
 
 async function findData(currentArgs, headlineModel) {
+  console.log('currentArgs', currentArgs);
+  console.log('headlineModel', headlineModel);
+  
 
   let query = {};
 
   for (let key in currentArgs) {
+    console.log(currentArgs);
+    console.log(key);
+    
     query[key] = currentArgs[key];
   }
 
-  if (query.keys().length) {
+  if (Object.keys(query).length) {
     return await headlineModel.find(query);
   } else {
     return await headlineModel.find({}).exec();
