08da62aa33d4c1dc423d89e8ed612d9a379caec9
eeden01 <mmvaneeden@gmail.com>
2020-03-06 15:43:46 +0100

    refactored scraper

---
+++ scraper/index.js
@@ -0,0 +1,103 @@
+const got = require('got');
+const cheerio = require('cheerio');
+
+async function getHeadline (url, path) {
+  try {
+    const response = await got(url);
+    const data = response.body;
+    const $ = cheerio.load(`${data}`);
+    //DATA-START
+    const headline = $(path).first().text();
+    // DATA-END
+    return headline;
+  } catch (error) {
+    console.log(error.response.body);
+  }
+};
+
+
+
+
+const newspapers = [{
+  newspaper: '20-minutos',
+  url: 'https://www.20minutos.es/',
+  path: 'section.board-b a[id="m54-53-55"]',
+  country: 'Spain'
+},
+{
+  newspaper: 'el-confidencial',
+  url: 'https://www.elconfidencial.com/',
+  path: 'section.opening-1-top h3.fs-60 a',
+  country: 'Spain'
+},
+{
+  newspaper: 'el-mundo-today',
+  url: 'https://www.elmundotoday.com/',
+  path: 'h3.entry-title  a',
+  country: 'Spain'
+},
+{
+  newspaper: 'el-mundo',
+  url: 'https://www.elmundo.es/',
+  path: 'div.ue-l-cover-grid__block div.size6of12 div.ue-l-cover-grid__unit article.ue-c-cover-content div.ue-c-cover-content__body div.ue-c-cover-content__main header.ue-c-cover-content__headline-group a.ue-c-cover-content__link h2.ue-c-cover-content__headline',
+  country: 'Spain'
+},
+{
+  newspaper: 'el-pais',
+  url: 'https://elpais.com/',
+  path: 'article.articulo--primero h2.articulo-titulo a',
+  country: 'Spain'
+},
+{
+  newspaper: 'el-periodico',
+  url: 'https://www.elperiodico.com/es/',
+  path: 'section.blockpad h2.fs3 a',
+  country: 'Spain'
+},
+{
+  newspaper: 'expansion',
+  url: 'https://www.expansion.com/',
+  path: 'h2.flex-article__heading a.flex-article__heading-link',
+  country: 'Spain'
+},
+{
+  newspaper: 'la-vanguardia',
+  url: 'https://www.lavanguardia.com/',
+  path: 'section.breakingnews div.col-xs-12 article.tpl-story-first div.story-bottom header.story-header h1.story-header-title a.story-header-title-link',
+  country: 'Spain'
+},
+{
+  newspaper: 'daily-mail',
+  url: 'https://www.dailymail.co.uk/home/index.html',
+  path: 'h2.linkro-darkred a',
+  country: 'UK'
+},
+{
+  newspaper: 'the-guardian',
+  url: 'https://www.theguardian.com/international',
+  path: '.fc-item__container a.js-headline-text',
+  country: 'UK'
+},
+{
+  newspaper: 'morning-start',
+  url: 'https://morningstaronline.co.uk/',
+  path: 'section.second h1',
+  country: 'UK'
+},
+{
+  newspaper: 'evening-standard',
+  url: 'https://www.standard.co.uk/?CMP=ILC-refresh',
+  path: '.hero-top h2',
+  country: 'UK'
+},
+{
+  newspaper: 'bbc-news',
+  url: 'https://www.bbc.com/',
+  path: 'li.media-list__item--1 a.media__link',
+  country: 'UK'
+},
+]
+
+
+exports.getHeadline = getHeadline;
+exports.newspapers = newspapers
