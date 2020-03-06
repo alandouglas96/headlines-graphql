68a9f5e1550b2bdfcf774bc2acb22fd097fdf840
eeden01 <mmvaneeden@gmail.com>
2020-03-06 14:22:22 +0100

    added filter

--- client/src/Components/HeadlineList/HeadLine-list.js
+++ client/src/Components/HeadlineList/HeadLine-list.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './style.css';
 import HeadLineItem from '../HeadLine-item/HeadLine-item';
 
-function HeadLineList ({headlines}) {
-  
+function HeadLineList ({ headlines }) {
+
   return (
     <div className="HeadLineList">
       <div className="header">Headlines around the world</div>
-      {headlines.map(headline => {
-        return <HeadLineItem headline={headline}/>
-   })}
+      <div className="filter">
+        <h3>Filter</h3>
+        <div>select country</div>
+        <div>select newspaper</div>
+      </div>
+      <div className="wrapper">
+        {headlines.map(headline => {
+          return <HeadLineItem headline={headline} />
+        })}
+      </div>
     </div>
   );
 }
