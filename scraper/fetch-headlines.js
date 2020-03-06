2ff7992df77589af860a17062d88d6f3ad46f63b
lluisemper <lluisemper@gmail.com>
2020-03-06 16:40:34 +0100

    refactor fecth in the scrapper

--- scraper/fetch-headlines.js
+++ scraper/fetch-headlines.js
@@ -1,18 +1,21 @@
 const moment = require('moment');
 const got = require('got');
 const cheerio = require('cheerio');
-const newspapers = require('./newspapers.js');
+const { newspapers } = require('./newspapers.js');
 
-const currentTime = Date.now();
+const currentTime = moment(Date.now());
+
 
 const headlines = {
   day: Number(currentTime.add(1, 'hours').format("DD")),
   month: Number(currentTime.add(1, 'hours').format("MM")),
   year: Number(currentTime.add(1, 'hours').format("YYYY")),
-  time: moment(currentTime.add(1, 'hours').format("LT")
+  time: moment(currentTime.add(1, 'hours').format("LT"))
 };
 
-async function getHeadline (url, path) {
+const headlines = {};
+
+async function getHeadline(url, path) {
   try {
     const response = await got(url);
     const data = response.body;
@@ -26,19 +29,22 @@ async function getHeadline (url, path) {
   }
 };
 
-async function fetchHeadlines () {
+async function fetchHeadlines() {
+  // console.log('newspaper', newspapers);
 
-  for (let i = 0; i < newspapers.length; i++) {
-    console.log(newspapers[i])
-  }
-
-  //   newspapers.map(async obj => {
-  //     headlines[obj.newspaper] = await getHeadline(obj.url, obj.path);
-  //   });
-  //   return headlines;
+  // for (let i = 0; i < newspapers.length; i++) {
+  //   console.log(newspapers[i])
   // }
 
+  await Promise.all(newspapers.map(async obj => {
+    headlines[obj.newspaper] = await getHeadline(obj.url, obj.path);
+  }));
+  console.log('headlines', headlines);
+  
+  return headlines;
 }
+
+
 
 fetchHeadlines();
 
